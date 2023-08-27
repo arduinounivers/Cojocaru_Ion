1c89a1770806e4a698ff712325f0ecd11fdb8757
Ion Cojocaru <30773190+arduinounivers@users.noreply.github.com>
2023-08-27 16:04:44 +0300

    Update botton.ino

--- botton.ino
+++ botton.ino
@@ -1,25 +1,18 @@
 void set_botton() {
-  Serial.print("Stare button:  ");
-  Serial.println(buttonState);
   cont = EEPROM.read(1);    
-  Serial.print("Cont:  ");
-  Serial.println(cont);   
   if (cont == 1) {     
     if (eprom == 1) { 
       EEPROM.write(1, 2);  
-      //delay(1000); 
      }   
     }
   if (cont == 2) {     
     if (eprom == 2) {
       EEPROM.write(1, 3);
-      //delay(1000); 
     }  
     }   
   if (cont == 3) {    
     if (eprom == 3) {
       EEPROM.write(1, 1);
-      //delay(1000);  
     }
   }
 }
